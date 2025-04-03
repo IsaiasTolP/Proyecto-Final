3f92e9863ab854ee7fc22c39ed121016830f36d6
IsaiasTolP <toledop.isaias@gmail.com>
2025-04-03 21:48:31 +0100

    Se avanza en los modelos

--- Crowdfunding/Backend/APP-API/projects/models.py
+++ Crowdfunding/Backend/APP-API/projects/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=75)
+    description = models.TextField()
+    goal = models.DecimalField(max_digits=10, decimal_places=2)
+    start_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
