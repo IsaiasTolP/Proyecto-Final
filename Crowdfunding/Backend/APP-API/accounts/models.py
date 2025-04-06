61187bb82e5fd01772147753872972fd2bc978ee
IsaiasTolP <toledop.isaias@gmail.com>
2025-04-06 19:48:58 +0100

    Se crean nuevos modelos

--- Crowdfunding/Backend/APP-API/accounts/models.py
+++ Crowdfunding/Backend/APP-API/accounts/models.py
@@ -6,3 +6,14 @@ class Profile(models.Model):
     bio = models.TextField(max_length=250, blank=True)
     pfp = models.ImageField(upload_to='profile_pics/', default='default.jpg')
     location = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+class FounderProfile(Profile):
+    website = models.URLField(max_length=200, blank=True)
+    social_media = models.JSONField(blank=True, default=dict)  # Store social media links as JSON
+    contact_email = models.EmailField(max_length=100, blank=True)
+
+    def __str__(self):
+        return f'{self.user.username} Founder Profile'
--- Crowdfunding/Backend/APP-API/projects/models.py
+++ Crowdfunding/Backend/APP-API/projects/models.py
@@ -1,10 +1,18 @@
 from django.db import models
 
+
+class ProjectCategory(models.Model):
+    category = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.category
+
 class Project(models.Model):
     name = models.CharField(max_length=75)
     description = models.TextField()
     goal = models.DecimalField(max_digits=10, decimal_places=2)
     start_date = models.DateField(auto_now_add=True)
+    category = models.ForeignKey(ProjectCategory, related_name='projects', on_delete=models.PROTECT)
 
     def __str__(self):
         return self.name
