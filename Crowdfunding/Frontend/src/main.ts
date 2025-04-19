7e6e1c0e6507e3e2018786e49ee9b038c48aa916
IsaiasTolP <toledop.isaias@gmail.com>
2025-04-19 16:05:30 +0100

    Se implementa bootstrap en el proyecto

--- Crowdfunding/Frontend/src/main.ts
+++ Crowdfunding/Frontend/src/main.ts
@@ -1,5 +1,8 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
+import '@/assets/css/bootstrap.min.css';
+// @ts-ignore
+import '@/assets/js/bootstrap.bundle.min.js';
 
 createApp(App).mount('#app')
