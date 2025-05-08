40af64345c20f89e9554cfad0803c4efc969cf53
Isaías <isaiaslol43@gmail.com>
2025-05-08 11:38:48 +0100

    Arreglo bug generar clave
    Se arregla un bug de generación de clave

--- Crowdfunding/Backend/APP-API/PaymentMethod/utils.py
+++ Crowdfunding/Backend/APP-API/PaymentMethod/utils.py
@@ -1,14 +1,19 @@
 from cryptography.fernet import Fernet
+import os
+
+KEY_PATH = 'secret.key'
 
 def generate_key():
     """
     Generate a key for encryption and decryption.
     """
     key = Fernet.generate_key()
-    with open('secret.key', 'wb') as key_file:
+    with open(KEY_PATH, 'wb') as key_file:
         key_file.write(key)
 
 def load_key():
+    if not os.path.exists(KEY_PATH):
+        generate_key()
     with open('secret.key', 'rb') as key_file:
         return Fernet(key_file.read())
     
