f89fa5cf817cc6c78cb909bae3bc4596d96c2ce4
Isaías <isaiaslol43@gmail.com>
2025-05-12 15:44:30 +0100

    Cambios con la base de datos en contenedor

--- Crowdfunding/Backend/APP-API/main/settings.py
+++ Crowdfunding/Backend/APP-API/main/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from datetime import timedelta
+import os
 from pathlib import Path
 from decouple import config
 
@@ -91,7 +92,8 @@ WSGI_APPLICATION = 'main.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'data', 'db.sqlite3'),
+        # NAME: BASE_DIR / 'db.sqlite3',
     }
 }
 
