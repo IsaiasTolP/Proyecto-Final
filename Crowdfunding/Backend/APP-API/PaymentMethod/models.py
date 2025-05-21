fa4850fe206cb4fc074406c71ff958dd3a69d725
IsaiasTolP <toledop.isaias@gmail.com>
2025-05-21 23:08:44 +0100

    Fix

--- Crowdfunding/Backend/APP-API/PaymentMethod/models.py
+++ Crowdfunding/Backend/APP-API/PaymentMethod/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.conf import settings
 from .utils import fernet
 from datetime import date
+from simple_history.models import HistoricalRecords
+
 
 class PaymentMethod(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='payment_methods', null=True)
@@ -9,20 +11,25 @@ class PaymentMethod(models.Model):
     card_number = models.BinaryField()
     cvv = models.BinaryField()
     expiration_date = models.DateField(null=True, blank=True)
+    history = HistoricalRecords()
 
-    def save_card(self, card_number, cvv):
-        if isinstance(card_number, str):
-            self.card_number = fernet.encrypt(card_number.encode())
-        elif isinstance(card_number, bytes):
-            self.card_number = card_number
-        if isinstance(cvv, str):
-            self.cvv = fernet.encrypt(cvv.encode())
-        elif isinstance(cvv, bytes):
-            self.cvv = cvv
-    
+    def encrypt_if_needed(self, value):
+        if isinstance(value, str):
+            return fernet.encrypt(value.encode())
+        elif isinstance(value, bytes):
+            return value
+        raise ValueError("Valor no válido para cifrado")
+
     def save(self, *args, **kwargs):
-        self.save_card(self.card_number, self.cvv)
+        self.card_number = self.encrypt_if_needed(self.card_number)
+        self.cvv = self.encrypt_if_needed(self.cvv)
         super().save(*args, **kwargs)
+
+    def get_card_number(self):
+        return fernet.decrypt(self.card_number).decode()
+
+    def get_cvv(self):
+        return fernet.decrypt(self.cvv).decode()
 
     def delete(self, *args, **kwargs):
         if self.payment_method_contributions.exists():
@@ -34,15 +41,12 @@ class PaymentMethod(models.Model):
         else:
             super().delete(*args, **kwargs)
 
-    def get_card_number(self):
-        return fernet.decrypt(self.card_number).decode()
-    
-    def get_cvv(self):
-        return fernet.decrypt(self.cvv).decode()
-    
     def __str__(self):
-        return f'**** **** **** {self.get_card_number()[-4:]} de {self.user}'
-    
+        try:
+            return f'**** **** **** {self.get_card_number()[-4:]} de {self.user}'
+        except Exception:
+            return f'Método de pago inválido de {self.user}'
+
     def is_expired(self):
         if not self.expiration_date:
             return True
